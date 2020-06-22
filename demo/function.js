048438ad459dbc6014265ffbb4a7699cc456e674
Nico <jnico.chilla@gmail.com>
2020-06-22 16:30:43 -0700

    CORS headers continue to cause issues...

--- demo/function.js
+++ demo/function.js
@@ -1,31 +1,50 @@
 
-var url='https://rss.art19.com/episodes/038a4b7b-fcd0-4e19-95bc-c0f2c44b9691.mp3';
 
+var url='https://rss.art19.com/episodes/038a4b7b-fcd0-4e19-95bc-c0f2c44b9691.mp3';
+//i think it's a problem with CORS because this URL below works fine.
+//https://s3-us-west-2.amazonaws.com/s.cdpn.io/858/outfoxing.mp3
+var AudioContext = window.AudioContext || window.webkitAudioContext;
+var context;
+var analyser;
+var source;
 
 
 function liveAudio(url) {
+  context=new AudioContext();
   var audiotag=document.querySelector('audio');
-  audiotag.src=url;
   audiotag.crossOrigin="anonymous";
-  var context=new (window.AudioContext || window.webkitAudioContext)();
-  var source=context.createMediaElementSource(audiotag);
+  audiotag.src=url;
+  source=context.createMediaElementSource(audiotag);
+  analyser=context.createAnalyser();
+  source.connect(analyser);
+  analyser.connect(context.destination);
+  analyser.fftSize = 32;
+
   console.log(context);
+
+
 
   //
   // fetch(url)
   //   .then(response => source=context.createMediaElementSource(response))
   // console.log(context);
-  // var analyser=context.createAnalyser();
-  // source.connect(analyser);
-  // analyser.connect(context.destination);
 
 
-  // analyser.fftSize = 32;
+
+
 
 }
+document.querySelector('body').addEventListener('click',loadit)
 
-window.addEventListener('load',loadit)
+
+// window.addEventListener('load',loadit)
+
 
 function loadit(){
-  liveAudio(url);
+  if(context==undefined){
+    liveAudio(url);
+  }else{
+    context.resume();
+    console.log(context,analyser);
+  }
 }
