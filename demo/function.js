f447c4657dcd6285a0ce2769124f558975ec273d
Nico <jnico.chilla@gmail.com>
2020-06-25 15:01:34 -0700

    removed cors proxy

--- demo/function.js
+++ demo/function.js
@@ -1,6 +1,6 @@
 
 
-var url='https://wnsr-cors.herokuapp.com/https://rss.art19.com/episodes/72a3bc7e-118a-4171-8be4-125913860ef7.mp3';
+var url='https://rss.art19.com/episodes/c08e9885-d882-4306-a4d4-bd7a7bd76db6.mp3';
 var audiotag=document.querySelector('audio');
 var AudioContext = window.AudioContext || window.webkitAudioContext;
 let OfflineAudioContext =window.OfflineAudioContext || window.webkitOfflineAudioContext;
