63fd892615e1de62649519e7e5ad145930ffc992
Nico <jnico.chilla@gmail.com>
2020-08-13 09:10:12 -0700

    added more checks

--- webkitdetect/detect.js
+++ webkitdetect/detect.js
@@ -2,11 +2,20 @@ function checks(){
   var nodeToPageTag=document.querySelector('#nodetopage');
   var isSafariTag=document.querySelector('#is-safari');
   var userAgentCheck=document.querySelector('#useragent');
-
+  var detailedUserAgentCheck=document.querySelector('#detaileduseragent');
 
   var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
 
   var uA = /Safari/.test(navigator.userAgent) && /Apple Computer/.test(navigator.vendor);
+
+  // var detailedUA=(navigator.userAgent.indexOf("Safari") !== -1 )
+
+  //
+  // if (navigator.userAgent.indexOf("Safari") !== -1 &&navigator.userAgent.indexOf("Chrome") == -1 &&navigator.userAgent.indexOf("Chromium") == -1) {
+  //   console.log("Safari");
+  // }else{
+  //   console.log("nope");
+  // }
 
   if(typeof window.webkitConvertPointFromNodeToPage === 'function'){
       nodeToPageTag.innerHTML='yes';
@@ -23,6 +32,11 @@ function checks(){
     userAgentCheck.innerHTML='yes';
   }else{
     userAgentCheck.innerHTML='no';
+  }
+  if(navigator.userAgent.indexOf("Safari") !== -1 &&navigator.userAgent.indexOf("Chrome") == -1 &&navigator.userAgent.indexOf("Chromium") == -1){
+    detailedUserAgentCheck.innerHTML='yes';
+  }else{
+    detailedUserAgentCheck.innerHTML='no';
   }
 }
 window.addEventListener('load',checks);
